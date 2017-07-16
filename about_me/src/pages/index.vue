ed74498957d6a468a664190b06b25f874bf7997f
liudongyun <liudongyun1215@sina.cn>
2017-07-17 01:48:41 +0800

    首次提交about_me文件

---
+++ about_me/src/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="index">
+		{{msg}}
+		<div class="lists_box">
+			<ul>
+				<li>content-1</li>
+				<li>content-2</li>
+				<li>content-3</li>
+			</ul>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+	  name: 'index',
+	  data(){
+	  	return{
+	  		msg:'当前是首页'
+	  	}
+	  }
+	}
+</script>
+<style scoped lang="less">
+	.lists_box{
+		position:fixed;
+		bottom:0;
+		width:100%;
+		ul{
+			display:flex;
+			justify-content:space-around;
+			li{
+				border:1px solid orange;
+				padding:0.5rem 1rem;
+			}
+		}
+	}
+	
+</style>
