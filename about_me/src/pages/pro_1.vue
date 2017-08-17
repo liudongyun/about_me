b8d6e706df1c1289af62f2d772a16658e0e240a7
liudongyun <liudongyun1215@sina.cn>
2017-08-17 18:04:47 +0800

    about_me新的提交

--- about_me/src/pages/pro_1.vue
+++ about_me/src/pages/pro_1.vue
@@ -1,58 +1,79 @@
 <template>
 	<div class="pro_1">
-		<swiper :options="swiperOption"  ref="mySwiper" class="swiper-box">  
-            <!-- 这部分放你要渲染的那些内容 -->  
-            <swiper-slide v-for="banner in banners" class="swiper-item">
-            	<img :src="banner">
-            </swiper-slide>   
-            <!-- 这是轮播的小圆点 -->  
-            <div class="swiper-pagination" slot="pagination"></div>  
-        </swiper>
+		<swiper :options="swiperOption_up" class="swiperOption_up">
+		    <swiper-slide v-for="banner in banners">
+		    	<img :src="banner">
+		    </swiper-slide>
+		  </swiper>
+
+		  <swiper :options="swiperOption_down" class="swiperOption_down">
+		    <swiper-slide v-for="banner in banners">
+		    	<img :src="banner">
+		    </swiper-slide>
+		  </swiper>
 	</div>
 </template>
-<script>                      
+<script>     
+	import '../assets/swiper.min.css'                 
 	import { swiper, swiperSlide } from 'vue-awesome-swiper'
 	export default {
 	  name: 'pro_1',
-	  components:{
-	  	swiper,
-	  	swiperSlide
+	  components: {
+	    swiper,
+	    swiperSlide
 	  },
 	  data(){
 	  	return{
 	  		msg:'当前是项目一页面',
-			banners: [ '../static/pic_1.jpg', '../static/pic_2.jpg'],
-			components: {
-			    swiper,
-			    swiperSlide
-			},
-	        swiperOption: {  
-	            //是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true  
-	            notNextTick: false,  
-	            pagination: '.swiper-pagination',  
-	            slidesPerView: 'auto',  
-	            centeredSlides: true,  
-	            paginationClickable: true,  
-	            spaceBetween:0,  
-	            direction : 'horizontal',
-	            onSlideChangeEnd: swiper => {  
-	                //这个位置放swiper的回调方法  
-	                this.page = swiper.realIndex+1;  
-	                this.index = swiper.realIndex;  
-	            }  
-            },
-            computed:{
-            	swiper(){
-            		return this.$refs.mySwiper.swiper;
-            	}
-            }
+			banners: [ '../static/pic_1.jpg', '../static/pic_2.jpg','../static/pic_3.jpg'],
+	        swiperOption_up: {
+	        	slidesPerView:1.5,
+	        	spaceBetween:38,
+	        	slidesOffsetBefore:66,
+	        	loop:true,
+	        	loopAdditionalSlides:1,
+	        	initialSlide:1,
+	        	control: ['swiperOption_down']
+	        },
+	        swiperOption_down: {
+	        	slidesPerView:1.5,
+	        	spaceBetween:38,
+	        	loop:true,
+	        	loopAdditionalSlides:1,
+	        	initialSlide:1,
+	        	control: ['swiperOption_up']
+
+	        }
+	    }
+	  },
+	  mounted(){
+	  	this.lunbo()
+	  },
+	  methods:{
+	  	lunbo(){
+	  		console.log('test')
+	  		let swiper_up = new Swiper('.swiperOption_up',{
+
+		  	})
+		  	let swiper_down = new Swiper('.swiperOption_down',{
+		  		initialSlide :2,
+		  	})
+	  		
+		  	
+		  	swiper_up.params.control = swiper_down 
 	  	}
+	  	
 	  }
 	}
 </script>
 <style scoped lang="less">
-	
-	.pro_1 .swiper-box>div{
-		display:flex;
+	.swiper-container{
+		height:400px;
+		width:100%;
+		margin-top:30px;
 	}
+	.swiper-container img{
+		width:100%;
+	}
+	
 </style>
